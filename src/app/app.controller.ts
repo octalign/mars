368a9c6cf903e02fcd986d76bba7ec10d7bf4f37
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2021-11-28 20:24:42 +0000

    refactor(app): move app files to own folder

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { AuthService } from '../auth/auth.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from '../auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
 
 describe('AppService', () => {
   let appService: AppService;
